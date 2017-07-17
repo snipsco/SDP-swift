b60b42966fa79ecce1fe8577058cf23fc6dce001
Sasha Lopoukhine <superlopuh@gmail.com>
2017-07-17 11:31:56 +0200

    generate project with swiftplate

---
+++ Sources/SimpleDatagramProtocol.swift
@@ -0,0 +1,9 @@
+//
+//  SimpleDatagramProtocol.swift
+//  Snips
+//
+//  Created by Sasha Lopoukhine on {TODAY}.
+//  Copyright © 2017 Snips. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/SimpleDatagramProtocolTests/SimpleDatagramProtocolTests.swift
@@ -0,0 +1,23 @@
+//
+//  SimpleDatagramProtocolTests.swift
+//  Snips
+//
+//  Created by Sasha Lopoukhine on {TODAY}.
+//  Copyright © 2017 Snips. All rights reserved.
+//
+
+import Foundation
+import XCTest
+import SimpleDatagramProtocol
+
+class SimpleDatagramProtocolTests: XCTestCase {
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        //// XCTAssertEqual(SimpleDatagramProtocol().text, "Hello, World!")
+    }
+    
+    static var allTests = [
+        ("testExample", testExample),
+    ]
+}
